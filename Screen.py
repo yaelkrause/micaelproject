54ae7febb0c19ed1c937015c745169259716d235
try1 <yaelkrause@gmail.com>
2026-08-16 14:07:33 +0300

    added stuff

--- Screen.py
+++ Screen.py
@@ -5,21 +5,43 @@ import random
 screen = pygame.display.set_mode(
         (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-def draw_bush():
-        bush_image = pygame.image.load(GRASS)
-        bush_image = pygame.transform.scale(bush_image, (60, 20))
+def generate_bush(field_screen):
+    bush_locations = []
 
-        for i in range(25):
-                random_x=random.randint(0,WINDOW_WIDTH)
-                random_y=random.randint(0,WINDOW_HEIGHT)
-                bush_image_rect = bush_image.get_rect(
-                        center=(random_x, random_y))
-                screen.blit(bush_image, bush_image_rect)
+    for bush in range(NUM_OF_LANDMINES):
+        x = random.randint(1, WINDOW_WIDTH)
+        y = random.randint(CELL_SIZE, WINDOW_HEIGHT)
+        bush_location = (x, y)
+
+        while bush_location in bush_locations:
+            x = random.randint(1, WINDOW_WIDTH - 3 * CELL_SIZE)
+            y = random.randint(CELL_SIZE, WINDOW_HEIGHT)
+            bush_location = (x, y)
+
+        bush_locations.append(bush_location)
+
+    return bush_locations
+
+def add_bushes_to_grid(bush_locations):
+    for bush in bush_locations:
+        col1 = calc_col(bush[0])
+        row1 = calc_row(bush[1])
+        for i in range(col1, col1+1):
+            field_screen[row1][i] = BUSH
+
+def calc_row(y):
+    return y//CELL_SIZE
+
+
+def calc_col(x):
+    return x//CELL_SIZE
 
 def create_field():
         return [[EMPTY for i in range(NUM_OF_COLS)] for j in range(NUM_OF_ROWS)]
 
-def draw_game(game_state):
+def draw_game(screen):
         screen.fill(FIELD_BACKGROUND_COLOR)
+        pygame.display.flip()
         global field_screen
         field_screen= create_field()
+        generate_bush(field_screen)
