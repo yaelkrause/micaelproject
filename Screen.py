27005424e4cf7dd89fabd238e5ff3873ce2190c6
try1 <yaelkrause@gmail.com>
2026-08-16 14:09:59 +0300

    added stuff

--- Screen.py
+++ Screen.py
@@ -28,6 +28,7 @@ def add_bushes_to_grid(bush_locations):
         row1 = calc_row(bush[1])
         for i in range(col1, col1+1):
             field_screen[row1][i] = BUSH
+        return field_screen
 
 def calc_row(y):
     return y//CELL_SIZE
@@ -39,9 +40,17 @@ def calc_col(x):
 def create_field():
         return [[EMPTY for i in range(NUM_OF_COLS)] for j in range(NUM_OF_ROWS)]
 
+def add_bushes(bush_locations):
+
 def draw_game(screen):
         screen.fill(FIELD_BACKGROUND_COLOR)
         pygame.display.flip()
         global field_screen
         field_screen= create_field()
-        generate_bush(field_screen)
+        field_screen=generate_bush(field_screen)
+
+bubble_image = pygame.image.load(bubble["image"])
+    bubble_image = pygame.transform.scale(bubble_image, (consts.BUBBLE_WIDTH, consts.BUBBLE_HEIGHT))
+    bubble_image_rect = bubble_image.get_rect(
+            center=(bubble["center_x"], bubble["center_y"]))
+    screen.blit(bubble_image, bubble_image_rect)
