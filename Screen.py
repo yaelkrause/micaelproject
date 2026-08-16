87481e93af83453a46ee261531a2e1cfccf4ea67
try1 <yaelkrause@gmail.com>
2026-08-16 11:22:17 +0300

    added stuff

--- Screen.py
+++ Screen.py
@@ -5,4 +5,4 @@ screen = pygame.display.set_mode(
         (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 def draw_game(game_state):
-        screen.fill(BACKGROUND_COLOR)
+        screen.fill(FIELD_BACKGROUND_COLOR)
