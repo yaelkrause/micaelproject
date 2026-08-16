81c9e2ab3cd947604e4af3766d14e560b6042dff
try1 <yaelkrause@gmail.com>
2026-08-16 11:53:32 +0300

    added stuff

---
+++ game_field.py
@@ -0,0 +1,18 @@
+from consts import *
+
+def create():
+    global game_grid
+    game_grid = [
+        create_row(row, row_start=0, row_length=consts.BUBBLE_GRID_COLS)
+        for row in
+        range(consts.BUBBLE_GRID_START_ROWS)]
+
+    # Create an empty row for future bubbles
+    last_row = consts.BUBBLE_GRID_START_ROWS
+    bubbles_grid.append(create_empty_row(last_row))
+
+def create_row(row_index, row_start, row_length):
+    return [bush.create(bush.calc_center_x(col, row_index, row_start),
+                          bush.calc_center_y(row_index),
+                          random.choice(consts.bubble_colors)) for col in
+            range(row_length)]
