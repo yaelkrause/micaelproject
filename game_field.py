f9375c49dbffa40c4c478b277bc39fb4458a00b0
jbt <karny4848@gmail.com>
2026-08-16 12:31:52 +0300

    updated grid

--- game_field.py
+++ game_field.py
@@ -4,9 +4,11 @@ from consts import *
 game_grid = []
 
 def create():
-    global game_grid
-
-
+    for row in range(NUM_OF_ROWS):
+        grid_row = []
+        for col in range(NUM_OF_COLS):
+            grid_row.append('empty')
+        game_grid.append(grid_row)
 
 def generate_landmines():
     landmines_locations = []
@@ -16,3 +18,15 @@ def generate_landmines():
         landmine_location = (x, y)
         while landmine_location in landmines_locations:
             landmines_locations.append(landmine_location)
+    return landmines_locations
+
+def calc_row(y):
+    return y//CELL_SIZE
+
+def calc_col(x):
+    return x//CELL_SIZE
+
+def add_landmines_to_grid(landmines_locations):
+    for mine in landmines_locations:
+        col = calc_col(mine[0])
+        row = calc_row(mine[1])
