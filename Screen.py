cbf7c3eeee0f0644c11da6326930260d4b33af6a
try1 <yaelkrause@gmail.com>
2026-08-16 12:25:17 +0300

    added stuff

--- Screen.py
+++ Screen.py
@@ -1,10 +1,12 @@
 from consts import *
 import pygame
+import random
 
 screen = pygame.display.set_mode(
         (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 def draw_bush(game_state):
+
 
 def draw_game(game_state):
         screen.fill(FIELD_BACKGROUND_COLOR)
--- game_field.py
+++ game_field.py
@@ -5,14 +5,8 @@ game_grid = []
 
 def create():
     global game_grid
-    game_grid = [
-        create_row(row, row_start=0, row_length=consts.BUBBLE_GRID_COLS)
-        for row in
-        range(consts.BUBBLE_GRID_START_ROWS)]
 
-    # Create an empty row for future bubbles
-    last_row = consts.BUBBLE_GRID_START_ROWS
-    bubbles_grid.append(create_empty_row(last_row))
+
 
 def generate_landmines():
     landmines_locations = []
@@ -22,10 +16,3 @@ def generate_landmines():
         landmine_location = (x, y)
         while landmine_location in landmines_locations:
             landmines_locations.append(landmine_location)
-
-
-def create_row(row_index, row_start, row_length):
-    return [bush.create(bush.calc_center_x(col, row_index, row_start),
-                          bush.calc_center_y(row_index),
-                          random.choice(consts.bubble_colors)) for col in
-            range(row_length)]
