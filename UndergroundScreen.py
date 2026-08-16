90fae619937cf8f9bc57b8db22ec87360c985ac8
jbt <karny4848@gmail.com>
2026-08-16 12:05:07 +0300

    updated some things in game

--- UndergroundScreen.py
+++ UndergroundScreen.py
@@ -4,7 +4,28 @@ from consts import *
 screen = pygame.display.set_mode(
         (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-def draw_screen():
+
+def calc_y(row):
+    return row * CELL_SIZE
+
+
+def calc_x(col):
+    return col * CELL_SIZE
+
+
+def draw_underground_screen():
     screen.fill(UNDERGROUND_BACKGROUND_COLOR)
+
     for row in range(NUM_OF_ROWS):
-        pygame.draw.line(screen, consts.BORDER_COLOR, start_pos=(0, ), end_pos=(consts.WINDOW_WIDTH, line_y))
+        pygame.draw.line(screen, UNDERGROUND_GRID_COLOR, start_pos=(0, calc_y(row)),
+                         end_pos=(WINDOW_WIDTH, calc_y(row)))
+
+    for col in range(NUM_OF_COLS):
+            pygame.draw.line(screen, UNDERGROUND_GRID_COLOR, start_pos=(calc_x(col), 0),
+                             end_pos=(calc_x(col), WINDOW_HEIGHT))
+
+    pygame.display.flip()
+    pygame.time.wait(1000)
+
+draw_underground_screen()
+pygame.quit()
--- game_field.py
+++ game_field.py
@@ -1,4 +1,7 @@
+import random
 from consts import *
+
+game_grid = []
 
 def create():
     global game_grid
@@ -10,6 +13,16 @@ def create():
     # Create an empty row for future bubbles
     last_row = consts.BUBBLE_GRID_START_ROWS
     bubbles_grid.append(create_empty_row(last_row))
+
+def generate_landmines():
+    landmines_locations = []
+    for mine in range(NUM_OF_LANDMINES):
+        x = random.randint(WINDOW_WIDTH//2, WINDOW_WIDTH - 3*CELL_SIZE)
+        y = random.randint(WINDOW_HEIGHT//2 - CELL_SIZE, WINDOW_HEIGHT)
+        landmine_location = (x, y)
+        while landmine_location in landmines_locations:
+            landmines_locations.append(landmine_location)
+
 
 def create_row(row_index, row_start, row_length):
     return [bush.create(bush.calc_center_x(col, row_index, row_start),
